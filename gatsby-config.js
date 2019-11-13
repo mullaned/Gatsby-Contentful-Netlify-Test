305b85121c872236e4205369d4cfc39a97f1963f
David Mullaney <david.mullaney@deptagency.com>
2019-11-13 16:52:08 +0000

    update test site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Contentful Netlify `,
+    description: `Test Project`,
+    author: `Dave`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
