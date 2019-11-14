17f152d64ec59704c6a8a28c725bb368f3723e61
David Mullaney <david.mullaney@deptagency.com>
2019-11-14 11:18:51 +0000

    gatsby plugins added

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,17 @@ module.exports = {
     title: `Gatsby Contentful Netlify `,
     description: `Test Project`,
     author: `Dave`,
+    siteUrl: `https://www.gatsbyjs.org`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-robots-txt',
+    `gatsby-plugin-sitemap`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/pages/`,
+        name: 'pages',
       },
     },
     `gatsby-transformer-sharp`,
@@ -36,6 +39,39 @@ module.exports = {
         spaceId: `i8rrklloxize`,
         accessToken: `AGKxm9a2JnvWyfi2fGWLfJwKLjA1gRPESAL57JOHqfc`
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 }
